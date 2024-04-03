758e06489ae7f7522979bf91249c7e413f82f643
bwesterg <benton.westergaard@gmail.com>
2024-04-03 17:32:10 -0600

    adds tests for editing hotel, and all tests are passing.

--- frontend/src/pages/EditHotel.tsx
+++ frontend/src/pages/EditHotel.tsx
@@ -2,9 +2,12 @@ import { useMutation, useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import * as apiClient from "../api-client";
 import ManageHotelForm from "../forms/ManageHotelForm/ManageHotelForm";
+import { useAppContext } from "../contexts/AppContext";
+
 
 const EditHotel = () => {
   const { hotelId } = useParams();
+  const { showToast } = useAppContext();
 
   const { data: hotel } = useQuery(
     "fetchMyHotelById", 
@@ -16,8 +19,12 @@ const EditHotel = () => {
   );
 
   const { mutate, isLoading } = useMutation(apiClient.updateMyHotelById, {
-    onSuccess: () => {},
-    onError: ()=>{}
+    onSuccess: () => {
+      showToast({ message: "Hotel Updated", type: "SUCCESS" })
+    },
+    onError: ()=>{
+      showToast({ message: "Could Not Save Hotel Changes", type: "ERROR"})
+    },
   });
 
   const handleSave = ( hotelFormData: FormData ) => {
