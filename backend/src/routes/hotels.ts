910cdf437a31a8423d0567af2979cc36af4f2d4e
bwesterg <benton.westergaard@gmail.com>
2024-04-10 15:52:15 -0600

    adds routes and FE code for hotel details page. Still needs some style tweaking and a guest info form.

--- backend/src/routes/hotels.ts
+++ backend/src/routes/hotels.ts
@@ -1,9 +1,13 @@
 import express, { Request, Response} from "express";
 import Hotel from "../models/hotel";
 import { HotelSearchResponse } from "../shared/types";
+import { param, validationResult } from "express-validator";
 
 // api/hotels/search?
 const router = express.Router();
+
+
+
 router.get("/search", async (req: Request, res: Response) => {
   try {
     // implement pagination b/c a search of thousands of pages
@@ -45,6 +49,25 @@ router.get("/search", async (req: Request, res: Response) => {
     res.status(500).json({ message: "Something isn't wokring" });
   }
 });
+
+// /api/hotels/3839854894390etc for info about hotel
+router.get("/:id", [
+  param("id").notEmpty().withMessage("Id associated with hotel required")
+], async (req: Request, res: Response)=> {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+
+  const id = req.params.id.toString();
+  try {
+    const hotel = await Hotel.findById(id);
+    res.json(hotel);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Could not fetch hotel" });
+  }
+})
 
 const constructSearchQuery = (queryParams: any) => {
   let constructedQuery: any = {};
--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -155,3 +155,11 @@ export const searchHotels = async (searchParams: SearchParams): Promise<HotelSea
   }
   return response.json();
 }
+
+export const fetchHotelById = async (hotelId: string): Promise<HotelType> => {
+  const response = await fetch(`${API_BASE_URL}/api/hotels/${hotelId}`);
+  if (!response.ok){
+    throw new Error("Problem with hotel fetch");
+  }
+  return response.json();
+};
